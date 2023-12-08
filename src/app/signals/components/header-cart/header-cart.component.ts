fef36268853c166cd644801df66d74432dff542f
hady <hady.wafa@hadafsolutions.com>
2023-12-08 11:29:10 +0200

    fix

--- src/app/signals/components/header-cart/header-cart.component.ts
+++ src/app/signals/components/header-cart/header-cart.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header-cart',
   templateUrl: './header-cart.component.html',
-  styleUrls: ['./header-cart.component.css']
 })
 export class HeaderCartComponent implements OnInit {
 
